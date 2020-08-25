f27ffad2ced7425c0ab13bac0db237226be6f0ff
Anthony McGrath <akm@anthonykyle.co.nz>
2020-08-25 15:16:36 +1200

    Finished Enspiraled and Made Pretty

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,89 @@
-import React from 'react'
+import React from "react";
+import Circle from "./Circle";
+import stringHash from 'string-hash'
 
-const App = props => {
-  const circle = {
-    cx: props.width / 2,
-    cy: props.height / 2,
-    level: 0,
-    r: 256
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      parent: false,
+      cx: props.width / 2,
+      cy: props.height / 2,
+      children: [],
+      level: 0,
+      r: props.radius || 256,
+    };
   }
 
-  return (
-    <svg width={props.width} height={props.height}>
-      <circle cx={circle.cx} cy={circle.cy} r={circle.r} />
-    </svg>
-  )
+  handleOver = (e) => {
+    console.log(e)
+    if (!e.state.parent) {
+      this.makeChildren(e);
+    }
+  };
+
+  makeChildren = (c) => {
+    c.setState(
+      {
+        parent: true,
+        children: [
+          // North Child
+          {
+            key:stringHash(c + '1'),
+            cx:c.state.cx,
+            cy:c.state.cy - c.state.r,
+            r: c.state.r / 2, 
+            level:c.state.level + 1,
+            handleOver: this.handleOver
+          },
+          // South Child
+          {
+            key:stringHash(c+2),
+            cx:c.state.cx,
+            cy:c.state.cy + c.state.r,
+            r:c.state.r / 2  ,
+            level:c.state.level + 1,
+            handleOver:this.handleOver
+          },
+          // East Child
+          {
+            key:stringHash(c+3),
+            cx:c.state.cx + c.state.r,
+            cy:c.state.cy,
+            r:c.state.r / 2  ,
+            level:c.state.level + 1,
+            handleOver:this.handleOver
+          },
+          // West Child
+          {
+            key:stringHash(c+4),
+            cx:c.state.cx - c.state.r,
+            cy:c.state.cy,
+            r:c.state.r / 2  ,
+            level:c.state.level + 1,
+            handleOver:this.handleOver
+          } 
+        ]
+      },
+      console.log(c)
+    );
+  };
+
+  render() {
+    return (
+      <svg width={this.props.width} height={this.props.height}>
+        <Circle
+          cx={this.state.cx}
+          cy={this.state.cy}
+          r={this.state.r}
+          children={this.state.children}
+          parent={this.state.parent}
+          handleOver={this.handleOver}
+        />
+      </svg>
+    );
+  }
 }
 
-export default App
+export default App;
---
+++ client/components/Circle.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import stringHash from "string-hash";
+
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000)
+    .toString(16)
+    .padStart(6, 0)}`;
+
+class Circle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      parent: props.parent || false,
+      children: props.children || [],
+      cx: props.cx || 128,
+      cy: props.cy || 128,
+      level: props.level || 0,
+      r: props.r || 256,
+      color: randomHexColor(),
+      opacity: 0,
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ opacity: 0.3 });
+    }, 0);
+  }
+  render() {
+    return (
+      <>
+        <circle
+          cx={this.state.cx}
+          cy={this.state.cy}
+          r={this.state.r}
+          onMouseOver={() => {
+            this.props.handleOver(this);
+          }}
+          style={{ fill: this.state.color, opacity: this.state.opacity }}
+        />
+        {this.state.children.map((child) => {
+          return (
+            <Circle
+              key={child.key}
+              parent={child.parent}
+              cx={child.cx}
+              cy={child.cy}
+              r={child.r}
+              handleOver={child.handleOver}
+            />
+          );
+        })}
+      </>
+    );
+  }
+}
+
+export default Circle;
